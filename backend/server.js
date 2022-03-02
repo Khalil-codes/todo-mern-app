39cddbd66d2c8a33915e8c7ae7ecc318a017133f
khalil-codes <khalilpati55@gmail.com>
2022-03-02 18:00:27 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middlewares/errorMiddleware");
@@ -12,6 +14,7 @@ connectDB();
 const app = express();
 
 // App Middleware
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
