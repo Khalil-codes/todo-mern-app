dd73ffd66655ae2c53d1b469ee77d8e328b0aa9c
khalil-codes <khalilpati55@gmail.com>
2022-03-02 13:37:05 +0530

    Added Todo CRUD using REST API

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,68 +1,91 @@
-const getTodos = (req, res) => {
+const Todo = require("../models/todoModel");
+const asyncHandler = require("express-async-handler");
+
+// @desc    Get goals
+// @route   GET /api/goals
+// @access  Private
+const getTodos = asyncHandler(async (req, res) => {
+    const todos = await Todo.find();
     res.status(200).json({
         status: "success",
-        result: 10,
+        result: todos.length,
         data: {
-            todos: "<Todos over here>",
+            todos,
         },
     });
-};
+});
 
-const createTodo = (req, res) => {
-    if (!req.body.title) {
+// @desc    Create goal
+// @route   POST /api/goals
+// @access  Private
+const createTodo = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400);
-        throw new Error("Please Add Title Field in the Body");
+        throw new Error("Please Add Text Field in the Body");
     }
+    const todo = await Todo.create({ text: req.body.text });
     res.status(201).json({
         status: "success",
         data: {
-            todo: `New Todo with Title: ${req.body.title}`,
+            todo,
         },
     });
-};
+});
 
-const getTodo = (req, res) => {
-    if (+req.params.id > 10) {
+// @desc    Get goal
+// @route   GET /api/goals/:id
+// @access  Private
+const getTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
         res.status(400);
         throw new Error(`Todo with ID ${req.params.id} not found`);
     }
     res.status(200).json({
         status: "success",
         data: {
-            todo: `<Todo ${req.params.id} over here>`,
+            todo,
         },
     });
-};
+});
 
-const updateTodo = (req, res) => {
-    if (+req.params.id > 10) {
+// @desc    Update goals
+// @route   PATCH /api/goals/:id
+// @access  Private
+const updateTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
         res.status(400);
         throw new Error(`Todo with ID ${req.params.id} not found`);
     }
-    if (!req.body.title) {
-        res.status(400);
-        throw new Error("Please Add Title Field in the Body");
-    }
+    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, {
+        completed: !todo.completed,
+    });
     res.status(200).json({
         status: "success",
         data: {
-            todo: `<Updated Todo ${req.params.id} over here>`,
+            todo: updatedTodo,
         },
     });
-};
+});
 
-const deleteTodo = (req, res) => {
-    if (+req.params.id > 10) {
+// @desc    Delete goals
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteTodo = asyncHandler(async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
         res.status(400);
         throw new Error(`Todo with ID ${req.params.id} not found`);
     }
+    await Todo.findByIdAndDelete(req.params.id);
     res.status(200).json({
         status: "success",
         data: {
-            id: +req.params.id,
+            id: req.params.id,
         },
     });
-};
+});
 
 module.exports = {
     getTodos,
